5b562e1242a57bcebc65bfdcdd55a6a0eefc5772
tomy_lucky <ase3577@gmail.com>
2021-02-21 15:02:01 +0600

    Changed behaviour for exceptionHandler

--- src/main/kotlin/com/example/assignment/Tamir/exception/RestResponseEntityExceptionHandler.kt
+++ src/main/kotlin/com/example/assignment/Tamir/exception/RestResponseEntityExceptionHandler.kt
@@ -11,12 +11,13 @@ class RestResponseEntityExceptionHandler : ResponseEntityExceptionHandler() {
 
     @ExceptionHandler
     fun handle(e: Exception): ResponseEntity<String> = when (e) {
+        is CardNumberAlreadyExistsException -> ResponseEntity(e.localizedMessage, HttpStatus.BAD_REQUEST)
         is CardNumberNotFoundByNumberException -> ResponseEntity(e.localizedMessage, HttpStatus.NOT_FOUND)
         is IncorrectPinCodeException -> ResponseEntity(e.localizedMessage, HttpStatus.NOT_FOUND)
         is CardNumberNotFoundByIDException -> ResponseEntity(e.localizedMessage, HttpStatus.NOT_FOUND)
         is IncorrectWithdrawAmount -> ResponseEntity(e.localizedMessage, HttpStatus.BAD_REQUEST)
         is NegativeAmountException -> ResponseEntity(e.localizedMessage, HttpStatus.BAD_REQUEST)
-        else -> ResponseEntity(e.localizedMessage, HttpStatus.NOT_FOUND)
+        else -> ResponseEntity(e.localizedMessage, HttpStatus.UNAUTHORIZED)
     }
 }
 
